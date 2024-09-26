d83be942ed21df3322278ca407462177037388b4
Kyaraazwa <kyaraazwa18@gmail.com>
2024-09-26 15:36:28 +0700

    Add files via upload

---
+++ Prak.PBO Pertemuan ke-4/Hierarchical/Dosen.java
@@ -0,0 +1,11 @@
+package Hierarchical;
+
+class Dosen extends Karyawan {
+    public Dosen(String nama) {
+        super(nama);
+    }
+
+    public void kerja() {
+        System.out.println("Ngajar");
+    }
+}
---
+++ Prak.PBO Pertemuan ke-4/Hierarchical/Staff.java
@@ -0,0 +1,12 @@
+package Hierarchical;
+
+class Staff extends Karyawan {
+    public Staff(String nama) {
+        super(nama);
+    }
+
+    public void kerja() {
+        System.out.println("membantu dosen");
+    }
+}
+
